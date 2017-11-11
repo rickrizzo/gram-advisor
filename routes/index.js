58cb04686a9e5835305144289f43f4f8cc5a080b
Robert Russo <rrusso44@icloud.com>
2017-11-11 17:00:42 -0500

    Bootstrap & JQuery!

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ ig.use({ access_token: '551122949.48102ae.9028c7ba15204bfe8132930e1a1c4c53' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ig.location_search({ lat: 42.7284, lng: -73.6918 }, function(err, result, remaining, limit) {
-    console.log(result);
-  });
+  // ig.location_search({ lat: 42.7284, lng: -73.6918 }, function(err, result, remaining, limit) {
+  //   console.log(result);
+  // });
   res.render('index', { title: 'Gram Advisor' });
 });
 
